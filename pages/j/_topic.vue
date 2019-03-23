8a7c8379f9be4a576fb788db301c1570cdf4bdc5
Spencer Young <spjy@hawaii.edu>
2019-03-23 09:30:56 -1000

    Initial commit

---
+++ pages/j/_topic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ markdown }}
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const markdown = await import(`~/assets/topics/${params.topic}.md`)
+
+    return { markdown: markdown }
+  }
+}
+</script>
+
+<style></style>
---
+++ plugins/kramed.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import kramed from 'kramed'
+
+Vue.prototype.$kramed = kramed
